cbdbe53073678e70fdc6bd00c4684eb6f1b08bac
Reyes <alreyes2381@gmail.com>
2025-09-28 15:08:00 -0700

    Update: Refactored the index.js toDo class and the FormValidation class

---
+++ components/FormValidator.js
@@ -0,0 +1,78 @@
+class FormValidator {
+  constructor(settings, formEl) {
+    this._settings = settings;
+    this._formEl = formEl;
+  }
+
+  _showInputError = (inputElement, errorMessage) => {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formEl.querySelector(errorElementId);
+    inputElement.classList.add(this._settings.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._settings.errorClass);
+  };
+
+  _hideInputError = (inputElement) => {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formEl.querySelector(errorElementId);
+    inputElement.classList.remove(this._settings.inputErrorClass);
+    errorElement.classList.remove(this._settings.errorClass);
+    errorElement.textContent = "";
+  };
+
+  _checkInputValidity = (inputElement) => {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  };
+
+  _hasInvalidInput = () => {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  };
+
+  _toggleButtonState = () => {
+    if (this._hasInvalidInput(this._inputList)) {
+      this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._settings.inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  };
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._settings.inputSelector)
+    );
+    this._buttonElement = this._formEl.querySelector(
+      this._settings.submitButtonSelector
+    );
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+
+  resetValidation() {
+    this._formEl.reset();
+    this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+  }
+}
+
+export default FormValidator;
---
+++ components/Todo.js
@@ -0,0 +1,65 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._selector = document.querySelector(selector);
+  }
+
+  _getDate() {
+    // // If a due date has been set, parsing this it with `new Date` will return a
+    // // number. If so, we display a string version of the due date in the todo.
+    const dueDate = new Date(this._data.date);
+    if (!isNaN(dueDate)) {
+      this._todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
+
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+
+    // // Apply id and for attributes.
+    // // The id will initially be undefined for new todos.
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _setEventListeners() {
+    // EventListener for checkbox
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+      console.log(this._data.completed);
+    });
+
+    // EventListener for delete button
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  getView() {
+    this._todoElement = this._selector.content
+      .querySelector(".todo")
+      .cloneNode(true);
+
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    // Assining the todoNameEl to this._data.name
+    this._todoNameEl.textContent = this._data.name;
+
+    // Clalling the mothods  this._generateCheckboxEl(); & this._setEventListeners();
+    this._getDate();
+    this._generateCheckboxEl();
+    this._setEventListeners();
+
+    return this._todoElement;
+  }
+}
+
+export default Todo;
